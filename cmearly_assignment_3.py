2cc85e1ebcb70c84da1e8b8d4602c202c1eb2ee2
cmearly-16 <cmearly@aggies.ncat.edu>
2025-09-24 17:16:28 -0400

    zyBooks Test Case 1: Verifies variable setup and basic program structure

---
+++ cmearly_assignment_3.py
@@ -0,0 +1,14 @@
+student_name = "Christopher Early"
+current_gpa = 3.50
+study_hours = input(int())
+social_points = input(int())
+stress_level = input(int())
+
+print(f"Welcome to my game, {student_name}!")
+print("")
+print(f" ----- Your current stats are: ----- ")
+print(f" Current GPA: {current_gpa}")
+print(f" Student Hours: {study_hours}")
+print(f" Social Points: {social_points}")
+print(f" Stress Level: {stress_level}")
+print(f"------------------------------------")
