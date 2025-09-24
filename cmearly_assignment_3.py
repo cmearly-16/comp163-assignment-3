bef9c156807bfb72f1174a283b5c26bba69a4307
cmearly-16 <cmearly@aggies.ncat.edu>
2025-09-24 17:36:05 -0400

    Add files via upload
    Verifies variable setup and basic program structure.

---
+++ cmearly_assignment_3.py
@@ -0,0 +1,17 @@
+student_name = "Christopher Early"
+current_gpa = 3.25
+study_hours = input(int())
+social_points = input(int())
+stress_level = input(int())
+
+print(f"Welcome to my game, {student_name}!")
+print("")
+print(f" ----- Your current stats are: ----- ")
+print(f" Current GPA: {current_gpa}")
+print(f" Student Hours: {study_hours}")
+print(f" Social Points: {social_points}")
+print(f" Stress Level: {stress_level}")
+print(f"------------------------------------")
+
+
+
